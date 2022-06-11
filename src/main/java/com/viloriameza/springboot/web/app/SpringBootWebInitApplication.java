476c1c436ad9eda6d0755bd25fdbdeb72f330b86
Manuel Viloria <mviloriameza@gmail.com>
2022-06-11 14:41:53 -0500

    Primer commit

---
+++ src/main/java/com/viloriameza/springboot/web/app/SpringBootWebInitApplication.java
@@ -0,0 +1,13 @@
+package com.viloriameza.springboot.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebInitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebInitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viloriameza/springboot/web/app/SpringBootWebInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viloriameza.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebInitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
